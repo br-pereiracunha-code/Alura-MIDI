1bf7a3bc94e2c0111bc0db489dddab289aa6525d
Bruno <pereiracode.br@gmail.com>
2022-07-01 22:37:13 -0300

    using anonymous function

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 //Ppm...............................
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
-
-listaDeTeclas[0].onclick = tocaSomPom;
 
 let contador = 0;
 
 //enquanto
 while(contador < listaDeTeclas.length){
-    listaDeTeclas[contador].onclick = tocaSomPom;
+    listaDeTeclas[contador].onclick = function(){
+        tocaSom('#son__tecla_pom');
+    }
     contador++;
 }
